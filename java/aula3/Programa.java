2f67b4c1b812ea9e6b46d95869c47b4f4875f4aa
jmd25454 <jmd25454@gmail.com>
2022-03-29 19:21:28 -0300

    update aula
    Update aula 3

--- java/aula3/Programa.java
+++ java/aula3/Programa.java
@@ -1,11 +1,12 @@
 package aula3;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Programa {
 
-  public static void main(String[] args){
+  public static void main(String[] args) throws FileNotFoundException{
 
     File arquivo = new File("C");
 
